bf7d47dc16190799e661259b184a8f3fcb1cc659
zhangxingcai <1140979607@qq.com>
2021-06-24 20:20:32 +0800

    add route

--- app/user/__init__.py
+++ app/user/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,render_template
 
 bp = Blueprint('user', __name__, url_prefix='/user')
 
@@ -6,16 +6,16 @@ bp = Blueprint('user', __name__, url_prefix='/user')
 @bp.route('/login')
 # as customer or as admin
 def login():
-    return 'login'
+     return render_template('user/login.html')
 
 
 @bp.route('/register')
 # name, email-id, password, contact number, and address
 #   as customer or as admin
 def register():
-    return 'register'
+   return render_template('user/register.html')
 
 
 @bp.route('/logout')
 def logout():
-    return 'logout'
+    return render_template('user/login.html')
