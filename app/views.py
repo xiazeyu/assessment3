9a3f5a22234f63fa7704cff3c033e46cae63237d
xiazeyu <xiazeyu_2011@126.com>
2021-06-24 19:52:01 +0800

    fixed error bug

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
+from . import admin, content, error, order, user
 from flask import Blueprint
 
 bp = Blueprint('main', __name__)
 
-from . import admin, content,error,order,user
+
 bp.register_blueprint(admin.bp)
 bp.register_blueprint(content.bp)
 bp.register_blueprint(error.bp)
 bp.register_blueprint(order.bp)
 bp.register_blueprint(user.bp)
-
