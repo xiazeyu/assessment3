5b2ad624699969786358f960a869e9161271da8d
xhl <1014431218@qq.com>
2021-06-24 18:56:37 +0800

    save work

--- auction/user/login_form.py
+++ auction/user/login_form.py
@@ -1,9 +1,8 @@
-from flask_wtf import FlaskForm as Form
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
- 
- 
-class LoginForm(Form):
-    accountNumber = StringField('accountNumber', validators=[DataRequired('accountNumber is null')])
-    password = PasswordField('password', validators=[DataRequired('password is null')])
+from wtforms.validators import DataRequired, EqualTo
+from flask_wtf import FlaskForm as FlaskForm
 
+class LoginForm(FlaskForm):
+    """登录表单类"""
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
---
+++ auction/user/test_login.py
@@ -0,0 +1,23 @@
+from auction.user.test_user import User
+from auction.user.forms import LoginForm, get_user
+from flask import render_template, redirect, url_for, request
+from flask_login import login_user
+# ...
+@app.route('/login/', methods=('GET', 'POST'))  # 登录
+def login():
+    form = LoginForm()
+    emsg = None
+    if form.validate_on_submit():
+        user_name = form.username.data
+        password = form.password.data
+        user_info = get_user(user_name)  # 从用户数据中查找用户记录
+        if user_info is None:
+            emsg = "Wrong username or password"
+        else:
+            user = User(user_info)  # 创建用户实体
+            if user.verify_password(password):  # 校验密码
+                login_user(user)  # 创建用户 Session
+                return redirect(request.args.get('next') or url_for('index'))
+            else:
+                emsg = "Wrong username or password"
+    return render_template('login.html', form=form, emsg=emsg)
