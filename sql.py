4306e348361f44a7d42577ce77426228c6c8d169
xuzhangying <2544505726@qq.com>
2021-06-26 16:32:59 +0800

    create event booking comment

--- sql.py
+++ sql.py
@@ -10,6 +10,58 @@ ctx.push()
 
 db.create_all()
 
+# create events  （may have some mistakes）
+event1 = Event(id='1',name='event1',type='type1',venue='square1',datetime='2021-7-1',price='500',artist='artist1',
+description='An interesting concert',ticketcount='7000',status='active')
+db.session.add(event1)
+
+event2 = Event(id='2',name='event2',type='type2',venue='square2',datetime='2021-7-7',price='400',artist='artist2',
+description='Forest Concert',ticketcount='8000',status='inactive')
+db.session.add(event2)
+
+event3 = Event(id='3',name='event3',type='type3',venue='square3',datetime='2021-6-30',price='300',artist='artist3',
+description='Strawberry Music Festival',ticketcount='10000',status='upcoming')
+db.session.add(event3)
+
+event4 = Event(id='4',name='event4',type='type2',venue='square4',datetime='2021-7-5',price='700',artist='artist4',
+description='Classical comcert',ticketcount='5000',status='active')
+db.session.add(event4)
+
+event5 = Event(id='5',name='event5',type='type1',venue='square2',datetime='2021-7-4',price='900',artist='artist5',
+description='Special comcert',ticketcount='5000',status='active')
+db.session.add(event5)
+
+db.session.commit()
+db.session.close()
+
+#create booking
+booking1 = Booking(datetime='',quantity='9',price='700',payment='6300',user_id=user3.id,event_id=event4.id)
+db.session.add(booking1)
+
+booking2 = Booking(datetime='',quantity='3',price='300',payment='900',user_id=user4.id,event_id=event3.id)
+db.session.add(booking2)
+
+booking3 = Booking(datetime='',quantity='5',price='400',payment='2000',user_id=user5.id,event_id=event2.id)
+db.session.add(booking2)
+
+
+db.session.commit()
+db.session.close()
+
+#create comment
+comment1=Comment(text='Good',creator_id=user3.id,event_id=event1.id)
+db.session.add(comment1)
+
+comment2=Comment(text='Perfect!',creator_id=user4.id,event_id=event1.id)
+db.session.add(comment2)
+
+comment3=Comment(text='Excellent!',creator_id=user5.id,event_id=event2.id)
+db.session.add(comment3)
+
+db.session.commit()
+db.session.close()
+
+
 # delete event
 delete from app.models import Booking, Event
 
@@ -41,7 +93,7 @@ def comment(id):
 # get comment by event_id
 Event.query.filter_by(event.id).all
 
-# remain tickets
+# remain tickets (unfinished)
 ticket_count = Event.query(ticketcount).filter_by(id=1)
 select sum(quantity) as sumvalue from Booking where 'event_id' = '1'
 
