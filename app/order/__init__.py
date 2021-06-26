8b9e1e8360d703e327367154cb02a4efa5bd8d76
xiazeyu <xiazeyu_2011@126.com>
2021-06-26 18:09:12 +0800

    remove booking result

--- app/order/__init__.py
+++ app/order/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, flash, redirect, url_for
 
 bp = Blueprint('order', __name__, url_prefix='/order')
 
@@ -12,4 +12,5 @@ def book():
 
 @bp.route('/booking_history')
 def booking_history():
-    return render_template('order/booking_history.html')
+    flash('booking successfuly.')
+    return redirect(url_for('main.content.list_items'))
