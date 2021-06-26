bc4423bef43efdf0486879605d3d2979a594b977
zhangxingcai <1140979607@qq.com>
2021-06-26 16:24:49 +0800

    fixed wrong

--- app/content/__init__.py
+++ app/content/__init__.py
@@ -15,14 +15,14 @@ def list_items():
      return render_template ('content/list_items.html')
 
 
-@bp.route('/details', methods=['GET', 'POST')
+@bp.route('/details', methods=['GET', 'POST'])
 # /details?event_id=<int> - detail page of the event
 #   image description date other
 def details():
      return render_template ('content/detials.html')
 
 
-@bp.route('/new_comment', methods=['GET', 'POST')
+@bp.route('/new_comment', methods=['GET', 'POST'])
 # /new_comment?event_id=<int> - new comment event handler
 #   author review date
 def new_comment():
