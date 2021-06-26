9006e5bc4c5b3ba4cd56926a3b9b52f48157ca11
xiazeyu <xiazeyu_2011@126.com>
2021-06-26 17:31:18 +0800

    event template

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -2,34 +2,68 @@ from flask import Blueprint, render_template, flash
 from flask_login import login_required, current_user
 from flask import redirect, url_for, request
 from .. import db
+from ..models import Event
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
 
-@bp.route('/create_event')
+@bp.route('/create_event', methods=['POST', 'GET'])
 @login_required
-#   Upcoming, Inactive, Booked, or Cancelled
 def create_event():
     if current_user.role != 'administrator':
-      return redirect(url_for('main.content.list_items'))
-    return render_template ('admin/event_editor.html')
+        return redirect(url_for('main.content.list_items'))
+    if request.method == 'POST':
+        new_event = Event(
+            name=request.form['name'],
+            type=request.form['type'],
+            venue=request.form['venue'],
+            datetime=request.form['datetime'],
+            price=request.form['price'],
+            artist=request.form['artist'],
+            description=request.form['description'],
+            ticketcount=request.form['ticketcount'],
+            status=request.form['status'],
+            creator_id=current_user.id,
+        )
+        db.session.add(new_event)
+        db.session.commit()
+        flash('New event added successfully.')
+        return redirect(url_for('main.admin.create_event'))
+    return render_template('admin/event_editor.html', e={}, title="Create new event")
 
 
-@bp.route('/update_event')
+@bp.route('/update_event', methods=['POST', 'GET'])
 @login_required
-# /admin/update_event?event_id=<int> - update event
 def update_event():
     if current_user.role != 'administrator':
-      return redirect(url_for('main.content.list_items'))
-    return render_template ('admin/event_editor.html')
+        return redirect(url_for('main.content.list_items'))
+    event_id = request.args.get('event_id')
+    e1 = Event.query.filter_by(id=event_id).first()
+    if request.method == 'POST':
+        new_event = Event(
+            name=request.form['name'],
+            type=request.form['type'],
+            venue=request.form['venue'],
+            datetime=request.form['datetime'],
+            price=request.form['price'],
+            artist=request.form['artist'],
+            description=request.form['description'],
+            ticketcount=request.form['ticketcount'],
+            status=request.form['status'],
+            creator_id=current_user.id,
+        )
+        # TODO
+        flash(f'#{event_id} updated successfully.')
+        return redirect(url_for('main.admin.update_event', event_id=event_id))
+    render_template('admin/event_editor.html', e=e1, title="Update the event")
 
 
 @bp.route('/delete_event')
 @login_required
-# /admin/delete_event?event_id=<int> - delete event
 def delete_event():
     if current_user.role != 'administrator':
-      return redirect(url_for('main.content.list_items'))
+        return redirect(url_for('main.content.list_items'))
     event_id = request.args.get('event_id')
-    flash()
-    return render_template ('content/list_items.html')
+    # TODO
+    flash(f'{event_id}# event deleted successfully.')
+    return redirect(url_for('main.content.list_items'))
