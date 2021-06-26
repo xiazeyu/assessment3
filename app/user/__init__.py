2b8759502c1645243d5cc918471e8bf71fa74814
xiazeyu <xiazeyu_2011@126.com>
2021-06-26 13:58:35 +0800

    finished user

--- app/user/__init__.py
+++ app/user/__init__.py
@@ -27,40 +27,42 @@ def login():
             return redirect(next or url_for('main.content.list_items'))
         else:
             flash(error)
-    return render_template('user/login.html', form=login_form)
+    return render_template('user/login.html', form=login_form, heading="Login")
 
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
-    username = request.args.get('username', None)
-    password = request.args.get('password', None)
-    email_id = request.args.get('email_id', None)
-    contact_number = request.args.get('contact_number', None)
-    address = request.args.get('address', None)
+    register_form = RegisterForm()
+    error = None
+    if register_form.validate_on_submit():
+        role = register_form.role.data
+        user_name = register_form.user_name.data
+        email_id = register_form.email_id.data
+        password = register_form.password.data
+        phonenumber = register_form.phonenumber.data
+        address = register_form.address.data
+        u1 = User.query.filter_by(username=user_name).first()
+        if u1 is not None:
+            error = 'user name alreday registered'
+        if error is None:
+            pwd_hash = generate_password_hash(password)
+            new_user = User(username=user_name,
+                            password_hash=pwd_hash,
+                            emailid=email_id,
+                            phonenumber=phonenumber,
+                            role=role,
+                            address=address,
+                            )
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('main.user.login'))
+        else:
+            flash(error)
 
-    return render_template('user/register.html')
-
-    form = RegisterForm()
-    if form.validate_on_submit():
-        print('Register form submitted')
-
-        # get username, password and email from the form
-        uname = form.username.data
-        pwd = form.password.data
-        email = form.email.data
-
-        # create password hash
-        pwd_hash = generate_password_hash(pwd)
-
-        # create a new user model object
-        new_user = User(name=uname, password_hash=pwd_hash, emailid=email)
-        db.session.add(new_user)
-        db.session.commit()
-        # commit to the database and redirect to HTML page
-        return redirect(url_for('auth.register'))
+    return render_template('user/register.html', form=register_form, heading="Register")
 
 
 @bp.route('/logout')
 def logout():
     logout_user()
-    return 'Successfully logged out user'
+    return redirect(url_for('main.content.list_items'))
